b4362114a99a3e1c910ec1f8913f159146f165f5
koushiksarkar <koushik.sarkar@e-arc.com>
2018-06-11 20:09:25 +0530

    added file storing functionality

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import Logger from './logger';
 dotenv.config();
 
 const log = new Logger();
-const logger = log.getLogger(process.env.LOG_LEVEL);
+let logDirPath;
+let logFileName;
+let logger = null;
 
+if(!process.env.NODE_ENV && process.env.LOG_LEVEL) {
+  logger = log.getLogger(process.env.LOG_LEVEL);
+} else {
+  if(process.env.LOG_DIR_PATH) {
+    logDirPath = process.env.LOG_DIR_PATH;  
+  }
+  if(process.env.LOG_FILE_INITIAL_NAME) {
+    logFileName = process.env.LOG_FILE_INITIAL_NAME;  
+  }
+  logger = log.getLoggerWithLogFilesSpecified(logDirPath, logFileName);
+}
 
 module.exports = {
   logger,
--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,6 @@
 import winston from 'winston';
+import appRoot from 'app-root-path';
+import fs from 'fs';
 
 class Logger {
   constructor() {
@@ -9,12 +11,30 @@ class Logger {
         new winston.transports.Console({
           handleExceptions: true,
           humanReadableUnhandledException: true,
-          timestamp: true,
-          json: true
+          timestamp: this.tsFormat,
+          formatter: this.logFormatter,
+          json: true,
+          level: 'debug'
         })
       ]
     });
   }
+
+  logFormatter(options) {
+    return `[${options.timestamp()}] - [${options.level.toUpperCase()}] -` + (options.message ? options.message : ``) +
+      (options.meta && Object.keys(options.meta).length ?
+        `\n\t` + JSON.stringify(options.meta) : ``);
+  };
+
+  tsFormat() {
+    const d = new Date();
+    var yyyy = d.getFullYear().toString();                                    
+    var mm = (d.getMonth()+1).toString(); // getMonth() is zero-based         
+    var dd  = d.getDate().toString();    
+    return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + ` ` + d.getHours() + `:` + d.getMinutes() + `:` +
+      d.getSeconds()
+  };
+
   setLogLevel(input) {
     if (input) {
       const logLevel = (`${input}`).toLowerCase();
@@ -27,10 +47,46 @@ class Logger {
       this.logger.transports.console.level = this.DEFAULT_LOG_LEVEL;
     }
   }
+
+  setLogFiles(filePath = `${appRoot}/LOG_STORAGE`, fileName = 'filelog') {
+    if (!fs.existsSync(filePath)) {
+      fs.mkdirSync(filePath);
+    }
+    this.logger.configure({
+      transports: [
+        new (winston.transports.File)({
+          name: 'info-file',
+          filename: `${filePath}/${fileName}-info.log`,
+          formatter: this.logFormatter,
+          timestamp: this.tsFormat,
+          level: 'info',
+          json : false
+        }),
+        new (winston.transports.File)({
+          name: 'error-file',
+          filename: `${filePath}/${fileName}-error.log`,
+          formatter: this.logFormatter,
+          timestamp: this.tsFormat,
+          level: 'error',
+          json : false
+        })
+      ]
+    });
+  }
   getLogger(logLevel) {
     this.setLogLevel(logLevel);
     return this.logger;
   }
+  getLoggerWithLogFilesSpecified(filePath, fileName) {
+    this.setLogFiles(filePath, fileName);
+    return this.logger;
+  }
+
+  getLoggerWithLogFiles() {
+    this.setLogFiles();
+    return this.logger;
+  }
+
 }
 
 export default Logger;
